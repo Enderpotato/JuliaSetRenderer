4dfea135d9e65045bc30ddb250e7b3b8bcc1996a
Enoch <lim_enoch@students.edu.sg>
2023-07-30 20:23:50 +0800

    MOOOORRRREEEE changes

--- JuliaSetRenderer/SFMLFuncsH.hpp
+++ JuliaSetRenderer/SFMLFuncsH.hpp
@@ -4,8 +4,6 @@ sf::Color newColor(float& r, float& g, float& b);
 
 sf::Color newColor(float& r, float& g, float& b, float& a);
 
-sf::Color Color(float& r, float& g, float& b, float& a);
-
 void RenderJuliaSet(
 	std::map<std::string, int> params,
 	const std::vector<int>& JuliaSet,
